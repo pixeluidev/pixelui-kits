d1ca7b6716abb468afad2085beee092fab29146f
Hadie Danker <hadie87@gmail.com>
2025-03-25 06:45:26 +0700

    tambah error 504

--- error-500/error-500.svelte
+++ error-500/error-500.svelte
@@ -1,11 +1,22 @@
-<div class="py-30 bg-gradient-to-bl from-blue-500 via-blue-800 to-blue-950">
-	<div class="container relative max-w-screen-lg pt-30">
-		<div class="text-[120px] md:text-[220px] text-center text-blue-100 leading-1 mb-10 font-black">500</div>
-		<div class="pt-[220px] relative z-10 sm:pt-[120px] flex flex-col max-w-screen-sm text-center mx-auto gap-6 items-center justify-center">
-			<h1 class="text-2xl md:text-4xl lg:text-6xl font-black text-blue-50">All of our servers are busy</h1>
-			<p class="text-blue-50">We cannot handle your request right now, please wait for a couple of minutes and refresh the page. Our team is already working on this issue.</p>
-			<div class="flex justify-center items-center">
-				<a href="/" class="border text-blue-500 bg-white px-4 py-2 rounded-md">Take me back to home page</a>
+<div class="bg-gradient-to-bl from-blue-500 via-blue-800 to-blue-950 py-30">
+	<div class="relative container max-w-screen-lg pt-30">
+		<div class="mb-10 text-center text-[120px] leading-1 font-black text-blue-100 md:text-[220px]">
+			500
+		</div>
+		<div
+			class="relative z-10 mx-auto flex max-w-screen-sm flex-col items-center justify-center gap-6 pt-[220px] text-center sm:pt-[120px]"
+		>
+			<h1 class="text-2xl font-black text-blue-50 md:text-4xl lg:text-6xl">
+				All of our servers are busy
+			</h1>
+			<p class="text-blue-50">
+				We cannot handle your request right now, please wait for a couple of minutes and refresh the
+				page. Our team is already working on this issue.
+			</p>
+			<div class="flex items-center justify-center">
+				<a href="/" class="rounded-md border bg-white px-4 py-2 text-blue-500"
+					>Take me back to home page</a
+				>
 			</div>
 		</div>
 	</div>
