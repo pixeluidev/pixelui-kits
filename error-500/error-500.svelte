4858d27356edc531bde9a3c6399421694ac869db
Hadie Danker <hadie87@gmail.com>
2025-03-25 05:09:26 +0700

    tambah module error

---
+++ error-500/error-500.svelte
@@ -0,0 +1,12 @@
+<div class="py-30 bg-gradient-to-bl from-blue-500 via-blue-800 to-blue-950">
+	<div class="container relative max-w-screen-lg pt-30">
+		<div class="text-[120px] md:text-[220px] text-center text-blue-100 leading-1 mb-10 font-black">500</div>
+		<div class="pt-[220px] relative z-10 sm:pt-[120px] flex flex-col max-w-screen-sm text-center mx-auto gap-6 items-center justify-center">
+			<h1 class="text-2xl md:text-4xl lg:text-6xl font-black text-blue-50">All of our servers are busy</h1>
+			<p class="text-blue-50">We cannot handle your request right now, please wait for a couple of minutes and refresh the page. Our team is already working on this issue.</p>
+			<div class="flex justify-center items-center">
+				<a href="/" class="border text-blue-500 bg-white px-4 py-2 rounded-md">Take me back to home page</a>
+			</div>
+		</div>
+	</div>
+</div>
--- index.ts
+++ index.ts
@@ -9,3 +9,6 @@ export { default as GridAsymmetrical } from './grid-asymmetrical/grid-asymmetric
 
 export { default as ErrorBasic404 } from './error-basic-404/error-basic-404.svelte';
 export { default as ErrorSimple404 } from './error-simple-404/error-simple-404.svelte';
+export { default as Error404AsBackgroundImage } from './error-404-as-background-image/error-404-as-background-image.svelte';
+export { default as Error503AsBackgroundImage } from './error-503-as-background-image/error-503-as-background-image.svelte';
+export { default as Error500 } from './error-500/error-500.svelte';
